8baadf4e6a650aa595cdbe8562de1e4ca0ad6dcb
Rauli Laine <rauli.laine@iki.fi>
2020-11-20 18:56:13 +0200

    Add example

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-// TODO: Add support for non-ASCII alphabetic characters.
-export const isIdentifierStart = (c: string): boolean => /^[$_a-zA-Z]$/.test(c);
-export const isIdentifierPart = (c: string): boolean => /^[$_a-zA-Z0-9]$/.test(c);
-export const isNewLine = (c: string): boolean => /^[\n\r]$/.test(c);
-export const isNumPart = (c: string): boolean => /^[0-9_]$/.test(c);
